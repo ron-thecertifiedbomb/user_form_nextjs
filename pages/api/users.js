2d7d2941d9d1f34dcf7014bb665f362f924c3ff0
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2023-04-25 11:29:23 +0800

    added boilerplate

---
+++ pages/api/users.js
@@ -0,0 +1,22 @@
+import clientPromise from "../../lib/mongodb";
+
+export default async (req, res) => {
+   try {
+       const client = await clientPromise;
+       const db = client.db("my_database");
+
+       const users = await db
+           .collection("users")
+           .find({})
+           .sort({ metacritic: -1 })
+           .limit(10)
+           .toArray();
+
+       res.json(users);
+   } catch (e) {
+       console.error(e);
+   }
+};
+
+
+
---
+++ pages/users.tsx
@@ -0,0 +1,47 @@
+import clientPromise from "../lib/mongodb";
+
+interface User {
+  _id: number;
+  firstname: string;
+  lastname: string;
+  dateofbirth: string;
+  age: number;
+}
+
+export default function Users({ users }: { users: User[] }) {
+  return (
+    <div>
+      {users.map((user) => (
+        <div key={user._id}>
+        
+          <h3>Name: {user.firstname} <span>{user.lastname}</span></h3>
+          <h4>Date of Birth: {user.dateofbirth}</h4>
+          <h4>Age: {user.age}</h4>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  try {
+    const client = await clientPromise;
+    const db = client.db("my_database");
+
+    const users = await db
+      .collection("users")
+      .find({})
+      .sort({ metacritic: -1 })
+      .limit(20)
+      .toArray();
+
+    return {
+      props: { users: JSON.parse(JSON.stringify(users)) },
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      props: { users: [] },
+    };
+  }
+}
