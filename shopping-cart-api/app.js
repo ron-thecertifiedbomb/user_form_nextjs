89aab1ecf89274654cb405e9dfd3828be23ec120
ronan sibunga <ronan.sibunga@veritaspay.com>
2023-07-05 15:58:06 +0800

    updated

--- shopping-cart-api/app.js
+++ shopping-cart-api/app.js
@@ -1,73 +1,82 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const multer = require('multer');
+const mongoose = require("mongoose");
+const multer = require("multer");
 
 app.use(express.json());
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-const mongoURI='mongodb+srv://Ronchiko:Mybabe0814@atlascluster.rjfmjfq.mongodb.net/my_cart_database?retryWrites=true&w=majority';
+const mongoURI =
+  "mongodb+srv://Ronchiko:Mybabe0814@atlascluster.rjfmjfq.mongodb.net/my_cart_database?retryWrites=true&w=majority";
 
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Error connecting to MongoDB:', err));
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 const cartItemSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   quantity: { type: Number, required: true, default: 1 },
-  photo: { type: String }, // Add a new field to store the photo filename or URL
+  photo: { type: String },
 });
 
-const CartItem = mongoose.model('CartItem', cartItemSchema, 'mycart');
+const CartItem = mongoose.model("CartItem", cartItemSchema, "mycart");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads'); // Specify the folder where uploaded files should be stored
+    cb(null, "uploads"); // Specify the folder where uploaded files should be stored
   },
   filename: function (req, file, cb) {
     // Generate a unique filename for the uploaded file
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + "-" + file.originalname);
   },
 });
 
 const upload = multer({ storage: storage });
 
-app.get('/api/mycart', async (req, res) => {
+app.get("/api/mycart", async (req, res) => {
   try {
     const cartItems = await CartItem.find();
     res.json(cartItems);
   } catch (err) {
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-app.post('/api/mycart', upload.single('photo'), async (req, res) => {
+app.post("/api/mycart", upload.single("photo"), async (req, res) => {
   try {
     const { name, price, quantity } = req.body;
 
-    if (typeof name !== 'string' || isNaN(parseFloat(price)) || isNaN(parseInt(quantity))) {
-      return res.status(400).json({ error: 'Invalid data format' });
+    if (
+      typeof name !== "string" ||
+      isNaN(parseFloat(price)) ||
+      isNaN(parseInt(quantity))
+    ) {
+      return res.status(400).json({ error: "Invalid data format" });
     }
 
-    const photo = req.file ? req.file.filename : '';
-    const newItem = new CartItem({ name, price: parseFloat(price), quantity: parseInt(quantity), photo });
+    const photo = req.file ? req.file.filename : "";
+    const newItem = new CartItem({
+      name,
+      price: parseFloat(price),
+      quantity: parseInt(quantity),
+      photo,
+    });
     const savedItem = await newItem.save();
     res.status(201).json(savedItem);
   } catch (err) {
-    console.error('Error creating cart item:', err); // Log the specific error
-    res.status(500).json({ error: 'Internal server error' }); // Return a 500 status code for internal server errors
+    console.error("Error creating cart item:", err); // Log the specific error
+    res.status(500).json({ error: "Internal server error" }); // Return a 500 status code for internal server errors
   }
 });
 
-
-app.put('/api/mycart/:id', async (req, res) => {
+app.put("/api/mycart/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const { name, price, quantity } = req.body;
@@ -77,24 +86,24 @@ app.put('/api/mycart/:id', async (req, res) => {
       { new: true }
     );
     if (!updatedItem) {
-      return res.status(404).json({ error: 'Item not found' });
+      return res.status(404).json({ error: "Item not found" });
     }
     res.json(updatedItem);
   } catch (err) {
-    res.status(400).json({ error: 'Bad request' });
+    res.status(400).json({ error: "Bad request" });
   }
 });
 
-app.delete('/api/mycart/:id', async (req, res) => {
+app.delete("/api/mycart/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const deletedItem = await CartItem.findByIdAndRemove(id);
     if (!deletedItem) {
-      return res.status(404).json({ error: 'Item not found' });
+      return res.status(404).json({ error: "Item not found" });
     }
     res.sendStatus(204);
   } catch (err) {
-    res.status(400).json({ error: 'Bad request' });
+    res.status(400).json({ error: "Bad request" });
   }
 });
 
