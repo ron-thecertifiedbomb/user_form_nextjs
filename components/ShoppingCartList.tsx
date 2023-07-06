d326162f51afa63c591159a4af6b47a3777ad767
ronan sibunga <ronan.sibunga@veritaspay.com>
2023-07-06 13:15:16 +0800

    added delete

--- components/ShoppingCartList.tsx
+++ components/ShoppingCartList.tsx
@@ -3,29 +3,22 @@ import { CartItem } from "../types/types";
 
 const ShoppingCartList = () => {
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
-  const [totalPrice, setTotalPrice] = useState<number>(0);
-
-  const fetchCartItems = async () => {
-    try {
-      const response = await fetch('http://localhost:5000/api/shopping_cart');
-      if (response.ok) {
-        const data = await response.json();
-        setCartItems(data);
-        calculateTotalPrice(data);
-      } else {
-        console.error('Failed to fetch cart items');
-      }
-    } catch (error) {
-      console.error('Network/server error:', error);
-    }
-  };
-
-  const calculateTotalPrice = (items: CartItem[]) => {
-    const total = items.reduce((acc, item) => acc + item.price, 0);
-    setTotalPrice(total);
-  };
 
   useEffect(() => {
+    const fetchCartItems = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/shopping_cart');
+        if (response.ok) {
+          const data = await response.json();
+          setCartItems(data);
+        } else {
+          console.error('Failed to fetch cart items');
+        }
+      } catch (error) {
+        console.error('Network/server error:', error);
+      }
+    };
+
     fetchCartItems();
   }, []);
 
@@ -36,10 +29,8 @@ const ShoppingCartList = () => {
       });
 
       if (response.ok) {
-        console.log('Successfully Deleted');
         const updatedCartItems = cartItems.filter((item) => item._id !== itemId);
         setCartItems(updatedCartItems);
-        calculateTotalPrice(updatedCartItems);
       } else {
         console.error('Failed to delete item from cart');
       }
@@ -55,11 +46,14 @@ const ShoppingCartList = () => {
         <div key={item._id}>
           <p>Name: {item.name}</p>
           <p>Price: {item.price}</p>
-          <div style={{ width: '200px' }}>{item.photo && <img src={`http://localhost:5000/uploads/${item.photo}`} alt={item.name} style={{ width: '100%' }} />}</div>
+          <p>Quantity: {item.quantity}</p>
+          <p>Item Total Price: ${item.price * item.quantity}</p>
+          <div style={{ width: '200px'}}>{item.photo && <img src={`http://localhost:5000/uploads/${item.photo}`} alt={item.name}  style={{ width: '100%'}}/>}</div>
           <button onClick={() => handleDeleteItem(item._id)}>Delete</button> {/* Button to delete the item */}
         </div>
       ))}
-      <p>Total Price: ${totalPrice}</p> {/* Display the total price */}
+
+      <p>Total Price: ${cartItems.reduce((total, item) => total + item.price * item.quantity, 0)}</p> {/* Display the total price */}
     </div>
   );
 };
