4e82cf38c3e870adc75ba9638c31041ca49991d2
ronan sibunga <ronan.sibunga@veritaspay.com>
2023-07-06 11:30:56 +0800

    added get method

--- shopping-cart-api/shoppingCart.js
+++ shopping-cart-api/shoppingCart.js
@@ -1,6 +1,5 @@
-
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
@@ -8,35 +7,48 @@ app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 
-
-const mongoURI = "mongodb+srv://Ronchiko:Mybabe0814@atlascluster.rjfmjfq.mongodb.net/my_cart_database?retryWrites=true&w=majority";
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+const mongoURI =
+  "mongodb+srv://Ronchiko:Mybabe0814@atlascluster.rjfmjfq.mongodb.net/my_cart_database?retryWrites=true&w=majority";
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    console.log('Connected to MongoDB Atlas');
+    console.log("Connected to MongoDB Atlas");
     app.listen(3004, () => {
-      console.log('Server started on port 3004');
+      console.log("Server started on port 3004");
     });
   })
   .catch((error) => {
-    console.error('Failed to connect to MongoDB Atlas:', error);
+    console.error("Failed to connect to MongoDB Atlas:", error);
   });
 
 // Define the cart item schema
-const cartItemSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  quantity: Number,
-  photo: String,
-}, { collection: 'shopping_cart' });
+const cartItemSchema = new mongoose.Schema(
+  {
+    name: String,
+    price: Number,
+    quantity: Number,
+    photo: String,
+  },
+  { collection: "shopping_cart" }
+);
 
+const CartItem = mongoose.model("CartItem", cartItemSchema);
 
-const CartItem = mongoose.model('CartItem', cartItemSchema);
+app.get("/api/shopping_cart", async (req, res) => {
+  try {
+    const cartItems = await CartItem.find();
+    res.json(cartItems);
+  } catch (error) {
+    console.error("Failed to fetch cart items:", error);
+    res.status(500).json({ error: "Failed to fetch cart items" });
+  }
+});
 
 // API endpoint to add a cart item
-app.post('/api/shopping_cart', async (req, res) => {
+app.post("/api/shopping_cart", async (req, res) => {
   const { name, price, quantity, photo } = req.body;
 
   try {
@@ -45,7 +57,7 @@ app.post('/api/shopping_cart', async (req, res) => {
 
     res.status(201).json(savedCartItem);
   } catch (error) {
-    console.error('Failed to save cart item:', error);
-    res.status(500).json({ error: 'Failed to save cart item' });
+    console.error("Failed to save cart item:", error);
+    res.status(500).json({ error: "Failed to save cart item" });
   }
 });
